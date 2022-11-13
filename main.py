f8912e1df3bf8d6719b38c486e2960cb1269c6b9
EverybodyHatesChris <81565951+EverybodyHatesChris@users.noreply.github.com>
2022-11-13 21:05:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -36,3 +36,43 @@ class działanie():
                     temp[j] = False
 
         return primes
+    
+    def standard_deviation(n):
+        
+    """V is list of variables (originally I needed velocity that's why v)
+       Vs is sum of variables
+       sumapoteg is sum of tbh idk what, I suck at math
+       n is ammount of variables to declare"""
+        import math as m
+        v = []
+        vs = 0
+        sumapoteg = 0
+        #n = int(input())
+
+
+        #creates list of variables
+        #I used float in case it was needed
+        i = 0
+        while i != n:
+            v.append(float(input()))
+            i += 1
+            print(i,n,v)
+
+        #sums up variables
+        #after loop vs becomes averege of variables
+        k = 0
+        while k != n:
+            vs += v[k]
+            k += 1
+        vs /= n
+
+        #now this one sums powers of idk some math bs
+        j = 0
+        while j != n:
+            sumapoteg += ((v[j]-vs)*(v[j]-vs))
+            j += 1
+
+        #print((vs, n, v, sumapoteg/n))
+
+        wynik = m.sqrt((sumapoteg/n))
+        return wynik
